87fd4338843b4eabfbf326a613817fd137af7ed8
felix <felix.bonin@polymtl.ca>
2020-01-18 14:45:49 -0500

    jkjn

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,9 @@
-package main.java;
+import java.io.IOException;
 
 public class Main {
 
-    
-    public static void main(String args[]) {
-        System.out.println("Hello world");
+    public static void main(String args[]) throws IOException {
+        APICaller apiCaller = new APICaller();
+        apiCaller.getLicensePaltes();
     }
 }
