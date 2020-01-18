fef6b4cd3e422a36389ec24e4638a5d3ced096e6
William Benoit <wilberben@hotmail.com>
2020-01-18 17:35:22 -0500

    htre

--- genetec.js
+++ genetec.js
@@ -1,10 +1,30 @@
-const express = require('express')
-const app = express();
+const { ServiceBusClient, ReceiveMode } = require("@azure/service-bus"); 
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// Define connection string and related Service Bus entity names here
+const connectionString = "Endpoint=sb://licenseplatepublisher.servicebus.windows.net/;SharedAccessKeyName=ConsumeReads;SharedAccessKey=VNcJZVQAVMazTAfrssP6Irzlg/pKwbwfnOqMXqROtCQ=";
+const topicName = "licenseplateread"; 
+const subscriptionName = "rVgrUNqGNtv7jVvU"; 
+
+async function main(){
+  const sbClient = ServiceBusClient.createFromConnectionString(connectionString); 
+  const subscriptionClient = sbClient.createSubscriptionClient(topicName, subscriptionName);
+  const receiver = subscriptionClient.createReceiver(ReceiveMode.receiveAndDelete);
+  var XHR = new XMLHttpRequest();
+
+  try {
+    const messages = await receiver.receiveMessages(10);
+    console.log("Received messages:");
+    console.log(messages.map(message => message.body));
+    XHR.open('POST', 'https://licenseplatevalidator.azurewebsites.net/api/lpr/platelocation', true, "equipe43", "WFynQsLZ3u7PYv22");
+    XHR.send(messages.body);
+    await subscriptionClient.close();
+  } finally {
+    await sbClient.close();
+  }
+}
+
+main().catch((err) => {
+  console.log("Error occurred: ", err);
 });
 
-app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
-});
